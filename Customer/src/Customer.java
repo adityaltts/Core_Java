82cb8676d8a5cdca2a903824e377c7e6195141e0
adityaltts <78849471+adityaltts@users.noreply.github.com>
2021-02-26 12:41:34 +0530

    Activity 2

---
+++ Customer/src/Customer.java
@@ -0,0 +1,13 @@
+
+public class Customer {
+
+	   private String details;
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+}
