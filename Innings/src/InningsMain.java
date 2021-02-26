533740b3d5fec74c801ed7289550faec3140545b
adityaltts <78849471+adityaltts@users.noreply.github.com>
2021-02-26 15:32:30 +0530

    Activity 2 Classes

---
+++ Innings/src/InningsMain.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InningsMain {
+	public static void main(String[] args) {
+				Scanner sc=new Scanner(System.in);
+				Innings in= new Innings();
+				System.out.println("Enter the team Name: ");
+				in.setTeamname(sc.nextLine());
+				System.out.println("Enter the Session: ");
+				in.setInningsname(sc.nextLine());
+				System.out.println("Enter runs: ");
+				in.setRuns(sc.nextInt());
+				
+			in.displayInningsDetails();
+			
+			sc.close();
+			}
+
+	}
