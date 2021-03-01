5bd3d0d0088934a5f49925f3f8af5047f3b49a59
adityaltts <78849471+adityaltts@users.noreply.github.com>
2021-03-01 11:52:20 +0530

    Day 5

---
+++ Day 5/Set Interface/HashSet/src/HashsetMain.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+public class HashsetMain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("No of Players: ");
+		int n = sc.nextInt();
+		
+		System.out.println("Enter a string: ");
+		String[] str = new String[10];    
+		Scanner sc1 = new Scanner(System.in);
+		for (int i = 0; i < n; i++)   
+		{  
+		str[i] = sc1.nextLine();
+		
+		}  
+		
+		 Set<String> hs = new HashSet<String>(); 
+		  for (int i = 0; i < n; i++)
+		  {
+		  hs.add(str[i]);
+		  }
+		  int size = hs.size();
+        System.out.println(size);
+
+		 
+	sc.close();
+	sc1.close();
+	
+	}
+	
+
+}
---
+++ Day 5/Set Interface/Player of the Match/src/PlayerName.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+public class PlayerName {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("No of Players: ");
+		int n = sc.nextInt();
+		
+		System.out.println("Enter a string: ");
+		String[] str = new String[10];    
+		Scanner sc1 = new Scanner(System.in);
+		for (int i = 0; i < n; i++)   
+		{  
+		str[i] = sc1.nextLine();
+		
+		}  
+		
+		 Set<String> hs = new TreeSet<String>(); 
+		  for (int i = 0; i < n; i++)
+		  {
+		  hs.add(str[i]);
+		  }
+		  Set<String> sorted = new TreeSet<String>(hs);
+		  System.out.println("Sorted Set: " + sorted);
+		 
+	sc.close();
+	sc1.close();
+	}
+}
