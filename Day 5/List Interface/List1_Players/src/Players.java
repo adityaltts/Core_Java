f2beb28ff3ef631deb761ab207ba02ed52200154
adityaltts <78849471+adityaltts@users.noreply.github.com>
2021-03-01 12:51:45 +0530

    Day 5

---
+++ Day 5/List Interface/List1_Players/src/Players.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Players {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		ArrayList<String> arr=new ArrayList<String>();//genesis
+		System.out.println("Enter the player details");
+		System.out.println("Enter player name");
+		String str= s.nextLine();
+		arr.add(str);
+		System.out.println("Enter age");
+		String var = s.nextLine();
+		arr.add(var);
+		System.out.println("Enter Country");
+		String str1= s.nextLine();
+		arr.add(str1);
+		System.out.println("Player details");
+		for(String h:arr)
+			System.out.println(h);
+		System.out.println("Enter Skill");
+		String str4= s.nextLine();
+		if(str4.equalsIgnoreCase("Batsman") || str4.equalsIgnoreCase("Bowler") || str4.equalsIgnoreCase("All Rounder")||str4.equalsIgnoreCase("Wicket Keeper") )
+		{
+			arr.add(str4);
+		}
+		System.out.println("Enter the position to add the skill");
+		int n1 = s.nextInt();
+		arr.set(n1,str4);
+		System.out.println("Player Details");
+		for(String h:arr)
+			System.out.println(h);
+		System.out.println("Enter the position of the detail to be removed");
+		int n = s.nextInt();
+		arr.remove(n);
+		System.out.println("Player details");
+		for(String h:arr)
+			System.out.println(h);
+		s.close();
+	}
+
+}
---
+++ Day 5/List Interface/List4_Count/src/Countmain.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Countmain {
+	public static void main(String[] args) {
+		int count=0,counte=0;
+		Scanner s=new Scanner(System.in);
+		ArrayList<Integer> arr=new ArrayList<Integer>();
+		int n = s.nextInt();
+		for(int i=0;i<n;i++)
+		{   int nn = s.nextInt();
+			arr.add(nn);
+		}
+		 for(int i=0;i<n;i++)
+		 { if(arr.get(i)>=50 && arr.get(i)<=99) 
+			 count++; 
+		 if(arr.get(i)>=100 && arr.get(i)<=199) 
+			 counte++; 
+		 }
+		 System.out.println(count);
+		 System.out.println(counte);
+		 s.close();
+	}
+}
